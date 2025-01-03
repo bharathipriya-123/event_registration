87e5d7215abe65cc5c0ae9c43d05a6c4d4c20fb4
bharathi20 <bharathipriya20112000@gmail.com>
2025-01-03 10:28:28 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
 import MultiStepForm from './components/MultiStepForm';
-import PlaceOrder from './components/PlaceOrder';
 
-import Stepper from './components/Stepper';
 
 
 function App() {
   return (
     <div>
      <MultiStepForm/>
-     {/* <Stepper/> */}
+    
     </div>
   );
 }
